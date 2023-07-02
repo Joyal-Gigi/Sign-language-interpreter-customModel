6b9e4d51bd09174112e24e58c7f2d3714fc790f1
JOYAL GIGI <108020550+Joyal-Gigi@users.noreply.github.com>
2023-07-02 07:31:34 +0530

    Update test_classifier.py
    added feature to make words from letters.
    solved a bug that creates an error when more than one hand id detected by limiting number of hands to one.

--- test_classifier.py
+++ test_classifier.py
@@ -16,10 +16,11 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
 # Initialize the hands module
-hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3, max_num_hands=1)
 
 #mapping corresponding labels to letters
 labels_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z'}
+wordString=""
 while True:
 
     data_aux = []
@@ -74,14 +75,29 @@ while True:
         # Get the predicted character based on the label
         predicted_character = labels_dict[int(prediction[0])]#matching predicted label to find letter
 
+        #add letter to string(to make words)
+        #press 'A' to add letter to word and 'c' to clear letter from word
+        if cv2.waitKey(1) == ord('a'):
+            wordString = wordString + predicted_character
+            print(wordString)
+        if cv2.waitKey(1) == ord('c'):
+            wordString = wordString[:-1]
+            print(wordString)
+
         # Draw the bounding box and predicted character on the frame
         cv2.rectangle(frame, (x1-50, y1-50), (x2+50, y2+50), (0, 0, 0), 4) #customising hud(rectangle and text)
         cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
                     cv2.LINE_AA)
+        cv2.putText(frame, "Word: " + wordString, (10,30), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
+                    cv2.LINE_AA)
     
     # Display the frame
     cv2.imshow('frame', frame)
-    cv2.waitKey(1)
+    
+    #press q to terminate
+    if cv2.waitKey(1) == ord('q'):
+        break
+
 
 # Release the video capture and close all windows
 cap.release()
